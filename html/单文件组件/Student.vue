c90cd40c0cb036e173adef4ef73cf3ccb5833767
xiewenwen <1121099970@qq.com>
2021-10-30 17:30:35 +0800

    ✨ vue-cli单文件组件

---
+++ html/单文件组件/Student.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="demo">
+        <h2>{{name}}</h2>
+        <h2>{{from}}</h2>
+    </div>
+</template>
+<script>
+
+    export default{
+        name:'Student',
+        data(){
+        return{
+            name:'ashin',
+            home:'zg'
+        }        
+    }
+    }
+</script>
+<style>
+ .demo{
+     background-color: brown;
+ }
+</style>
